41e084c37817ad127a37211909a53d4ba123b131
Jocelyn Berenice Zamora De La Cerda <201G0293@rcarbonifera.tecnm.mx>
2024-05-03 22:17:03 -0600

    Agregar archivos de proyecto.

---
+++ ChatApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ChatApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly HubConnection _connection;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://192.168.1.67:5296/chat")
+                .Build();
+
+            _connection.On<string>("MensajeResivido", (message) =>
+            {
+                chatMessage.Text += $"{Environment.NewLine}{message}";
+            });
+
+            Task.Run(() =>
+            {
+                Dispatcher.Dispatch(async () =>
+                await _connection.StartAsync());
+            });
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            await _connection.InvokeCoreAsync("EnviarMensaje", args: new[] { myChatMessage.Text });
+
+            myChatMessage.Text = String.Empty;
+        }
+    }
+}
---
+++ MAUISignalRSample.Server/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MAUISignalRSample.Server
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            Console.WriteLine(message);
+            await Clients.All.SendAsync("MensajeResivido", message);
+        }
+    }
+}
